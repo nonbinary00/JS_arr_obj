03b64dc266515be4db964ba42561289ffc36afb1
nonbinary00 <agarkovaalysh12@gmail.com>
2025-03-27 16:08:54 +0100

    New checks
    Added new condition checks in the number of viewed movies via conditions

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-const numberOfFilms = +prompt('How many movies have you watched already?', ''); 
+const numberOfFilms = +prompt('How many movies have you watched already?', '');
 // prompt() показывает диалоговое окно с вопросом.
 // перед prompt преобразует ответ в число (например, строка "5" станет числом 5)
 // Результат сохраняется в переменной numberOfFilms
-
-
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -12,7 +10,6 @@ const personalMovieDB = {
   genres: [],
   privat: false,
 };
-
 
 // const a = prompt('One of the last movies you watched?', ''),
 //   b = prompt('How do you rate the film?', ''),
@@ -25,11 +22,20 @@ const personalMovieDB = {
 // Название первого фильма (a) становится ключом, а его оценка (b) - значением
 // Название второго фильма (c) становится ключом, а его оценка (d) - значением
 
-for( let i = 0; i < 2; i++){
-  a = prompt('One of the last movies you watched?', ''),
-  b = prompt('How do you rate the film?', '');
+for (let i = 0; i < 2; i++) {
+  (a = prompt('One of the last movies you watched?', '')),
+    (b = prompt('How do you rate the film?', ''));
   personalMovieDB.movies[a] = b;
 }
 
-console.log(personalMovieDB);
+if (personalMovieDB.count < 10) {
+  console.log('Few films watched');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('You are a movie lover!');
+} else if (personalMovieDB.count >= 30) {
+  console.log('You love movie!');
+} else {
+  console.log('Error');
+}
 
+console.log(personalMovieDB);
