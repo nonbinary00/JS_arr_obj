2cc19a84de0e72bfeb330e311eb843065f0c338f
nonbinary00 <agarkovaalysh12@gmail.com>
2025-03-27 13:57:26 +0100

    Using the cycle
    Rewrote the questions for the cycle and added comments

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const numberOfFilms = +prompt('How many movies have you watched already?', '');
+const numberOfFilms = +prompt('How many movies have you watched already?', ''); 
+// prompt() показывает диалоговое окно с вопросом.
+// перед prompt преобразует ответ в число (например, строка "5" станет числом 5)
+// Результат сохраняется в переменной numberOfFilms
+
+
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,12 +13,23 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt('One of the last movies you watched?', ''),
-  b = prompt('How do you rate the film?', ''),
-  c = prompt('One of the last movies you watched?', ''),
-  d = prompt('How do you rate the film?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// const a = prompt('One of the last movies you watched?', ''),
+//   b = prompt('How do you rate the film?', ''),
+//   c = prompt('One of the last movies you watched?', ''),
+//   d = prompt('How do you rate the film?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// Создаются свойства в объекте movies:
+// Название первого фильма (a) становится ключом, а его оценка (b) - значением
+// Название второго фильма (c) становится ключом, а его оценка (d) - значением
+
+for( let i = 0; i < 2; i++){
+  a = prompt('One of the last movies you watched?', ''),
+  b = prompt('How do you rate the film?', '');
+  personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB);
+
